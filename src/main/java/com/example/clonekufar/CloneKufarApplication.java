6d13679f57cc11eb4566c807a668666911f1aa7f
“Andrei <Snayper1234@yandex.ru>
2023-04-20 00:24:31 +0300

    add

---
+++ src/main/java/com/example/clonekufar/CloneKufarApplication.java
@@ -0,0 +1,13 @@
+package com.example.clonekufar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneKufarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneKufarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clonekufar/CloneKufarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clonekufar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneKufarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
